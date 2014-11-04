7273811aed56a8a4cd951ba41237a78fef7a3899
chris15001900 <krzysztofbaczkiewicz@gmail.com>
2014-11-04 10:50:43 +0100

    minor change

--- src/views/show.blade.php
+++ src/views/show.blade.php
@@ -5,7 +5,7 @@
 	<h3 class="pull-left">Wiadomość #{{ $message->id }}</h3>
 	<a href="{{ url('admin/contact') }}" class="btn btn-lg btn-warning pull-right">Powrót do wiadomości</a>
 
-	<a class="btn btn-lg btn-primary pull-right" href="mailto:{{ $message->email }}?subject=Odpowiedź%20ze%20strony%20{{ url('/')}}&body=Twoja%20wiadomość:%0A%0A{{$message->content}}%0A%0A%0A%0AOdpowiedź:%0A%0A">
+	<a class="btn btn-lg btn-primary pull-right" href="mailto:{{ $message->email }}?subject=Odpowiedź%20ze%20strony%20{{ url('/')}}&body=Twoja%20wiadomość:%0A%0A{{$message->content}}%0A%0A%0A%0AOdpowiedź:%0A%0A"></a>
 
 	<br>
 
